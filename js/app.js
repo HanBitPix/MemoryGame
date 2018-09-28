aa0c41b844ac01c02d924073056a424f067693e2
HanBitPix <hanjt517@gmail.com>
2018-09-28 13:04:18 -0700

    Updated: Modal to show player info and fixed few issues

--- js/app.js
+++ js/app.js
@@ -44,47 +44,91 @@ function shuffle(array) {
 
 // Clicked Cards (Show)
 const clickedCards = (card) => {
-	openedCards.push(card);
-	card.classList.add('open', 'show');
+  openedCards.push(card);
+  card.classList.add('open', 'show');
 }
 
 // MATCH
 // Function to use when cards are matched to stay open
 const matchedCards = () => {
 
-	// Clicked 1st card
-	openedCards[0].classList.add('match', 'open', 'show');
-	// Clicked 2nd card
-	openedCards[1].classList.add('match', 'open', 'show');
-	// Empty out opened cards so user can go click again
-	numberMatches += 1;
-	openedCards = [];
-	counter += 1;
-	// Counter for every 2 flip
+  // Clicked 1st card
+  openedCards[0].classList.add('match', 'open', 'show');
+  // Clicked 2nd card
+  openedCards[1].classList.add('match', 'open', 'show');
+  // Empty out opened cards so user can go click again
+  numberMatches += 1;
+  openedCards = [];
+  counter += 1;
+  // Counter for every 2 flip
 	
 
-	// If total 8 matches than this alert will show
-	if (numberMatches == 8){
-		setTimeout(() => {
-			alert('You watched them all!');
-			clearTimeout(seconds);
-		}, 1000);
-	}
+  // If total 8 matches than this alert will show
+  if (numberMatches == 2){
+
+    // 
+    setTimeout(() => {
+      // Gets the end time after matching all
+      const totalTime = document.querySelector('.timer').textContent;
+      document.querySelector('.totalTime').textContent = totalTime;
+
+      // Stars left
+      const starsLeft = document.querySelectorAll('.star').length;
+      document.querySelector('.totalStars').textContent = starsLeft;
+
+      // Total Movies
+      const totalMoves = document.querySelector('.moves').textContent;
+      document.querySelector('.totalMoves').textContent = totalMoves;
+
+      // Resets the time
+      clearTimeout(seconds);
+
+      //** Start of Modal **//
+      // Get the modal
+      const modal = document.getElementById('myModal');
+
+      // Get the span element that closes the modal
+      const close = document.getElementsByClassName("close")[0];
+
+      // Open the Modal
+			modal.style.display = "block";
+			
+			// Play again button
+			const playAgain = document.getElementById('playAgain');
+			playAgain.addEventListener('click', () => {
+				location.reload();
+			});
+	
+      // When the user clicks on <span> (x), close the modal
+      close.onclick = function() {
+    	modal.style.display = "none";
+      }
+
+      // When the user clicks anywhere outside of the modal, close it
+      window.onclick = function(event) {
+        if (event.target == modal) {
+          modal.style.display = "none";
+    	}
+      } // End of Modal	
+
+
+    }, 1000);
+  }
 };
 
 // NOT MATCH
 // Function to use when cards are NOT matched
 const notMatchCards = () => {
-	setTimeout(() => {
-		openedCards.forEach((card) => {
-			card.classList.remove('open', 'show');
-		});
-		// Empty out opened cards so user can go click again
-		openedCards = [];
+  setTimeout(() => {
+    openedCards.forEach((card) => {
+      card.classList.remove('open', 'show');
+    });
+    // Empty out opened cards so user can go click again
+    openedCards = [];
 
-		// Counter for every 2 flip
-	}, 500);
-	counter += 1;
+    // Counter for every 2 flip
+  }, 500);
+  counter += 1;
 };
 
 /*
@@ -103,7 +147,7 @@ const initGame = () => {
   const cardHTML = shuffle(cards).map((card) => {
     return generateCard(card);
   });
-	deck.innerHTML = cardHTML.join('');
+  deck.innerHTML = cardHTML.join('');
 };
 
 initGame();
@@ -124,35 +168,35 @@ allCards.forEach((card) => {
   card.addEventListener('click', () => {
     if (!card.classList.contains('open', 'show', 'open')){
 
-			// When cards are clicked, this function will be used
-			clickedCards(card);
+      // When cards are clicked, this function will be used
+      clickedCards(card);
 
       // After 2 card clicks
       if (openedCards.length == 2){
 
         // Check if the two cards clicked, matches by the data in the class
         if (openedCards[0].dataset.card == openedCards[1].dataset.card){
-					// When cards are matched, this function will be used
-					matchedCards();
-				} else {
-					// When cards are not matched, this function will be used
-					notMatchCards();
-				} 
-				// Changes the score moves +1
-				moves.textContent = counter;
+          // When cards are matched, this function will be used
+          matchedCards();
+        } else {
+          // When cards are not matched, this function will be used
+          notMatchCards();
+        } 
+        // Changes the score moves +1
+        moves.textContent = counter;
 
-				// Removes a star after every # of moves
-				starRating(counter);
+        // Removes a star after every # of moves
+        starRating(counter);
       } // END : if (openedCards.length == 2){
-		} // END : if (!card.classList.contains('open', 'show', 'open'))
+    } // END : if (!card.classList.contains('open', 'show', 'open'))
   }); // END : card.addEventListener('click', () => {
 }); // END : allCards.forEach((card) =>
 
-// When clicked are restart, resets the game
+// When clicked on restart, resets the game
 const restart = document.querySelector('.restart');
 
 restart.addEventListener('click', () => {
-	location.reload();
+  location.reload();
 });
 
 // Star Rating Function
@@ -160,17 +204,17 @@ const stars = document.querySelector('.stars');
 const star = document.querySelectorAll('.star');
 
 const starRating = (counter) => {
-	if (counter == 8){
-		stars.removeChild(star[0]);
-	}
+  if (counter == 8){
+    stars.removeChild(star[0]);
+  }
 
-	if (counter == 12){
-		stars.removeChild(star[1]);
-	}
+  if (counter == 12){
+    stars.removeChild(star[1]);
+  }
 
-	if (counter == 16){
-		stars.removeChild(star[2]);
-	}
+  if (counter == 16){
+    stars.removeChild(star[2]);
+  }
 };
 
 // Timer Start
@@ -178,9 +222,9 @@ let start = 0;
 let seconds;
 
 let timer = () => {
-	document.querySelector('.timer').textContent = start;
-	start += 1;
-	seconds = setTimeout(timer, 1000);
+  document.querySelector('.timer').textContent = start;
+  start += 1;
+  seconds = setTimeout(timer, 1000);
 };
 
 timer();
